2ebb129df152f688db6066ee16e1652da1ae375d
Gui-pin <guilhermepinheiro082005@gmail.com>
2022-06-01 08:42:19 -0500

    alterações1

--- trab_pratica_em_programacao/form/validar_form.php
+++ trab_pratica_em_programacao/form/validar_form.php
@@ -1,5 +1,17 @@
 <?php
 
+$server = 'localhost';
+$bdname = 'cadastro';
+$user = 'root';
+$pass = 'root';
+
+$conexao = mysqli_connect($server, $user, $pass, $bdname);
+if(!$conexao)
+{
+    die('Não foi possível se conectar ao banco de dados!')
+    echo mysqli_error($conexao)
+}
+
 $cores = $_REQUEST['check'];
 if (!empty($cores)) {
     echo '<b>Cores escolhidas:</b><br><br>';
